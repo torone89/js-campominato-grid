e822fcc52f6115ff229568b97d52d5fb73336e05
torone89 <torone89>
2022-06-30 14:30:21 +0200

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@
 // VERIFICA COLLEGAMENTO JAVA
 console.log("JS")
 
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora 
+// di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 // # MILESTONE 1
 // Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
+
+
 // #MILESTONE 2
 // Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click
 //  del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
